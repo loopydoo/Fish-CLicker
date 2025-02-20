26ca8fc81a9206d5037f0ceed073346b4878bca6
loopydoo <140463891+loopydoo@users.noreply.github.com>
2025-02-21 10:37:18 +1300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 var score = 0
 var scoreAdder = 1
+var cursorPrice = 10
 document.getElementById("score").innerText = score + " Fish"
 function add() {
     score += scoreAdder
+    score = Math.round(score*10)/10
     document.getElementById("score").innerText = score + " Fish"
+}
+
+function buyCursor() {
+    if (score >= cursorPrice) {
+        score = score - cursorPrice
+        
+        cursorPrice *= 1.1
+        cursorPrice = Math.round(cursorPrice*10)/10
+        scoreAdder *= 1.11
+        scoreAdder = Math.round(scoreAdder*10)/10
+        score = Math.round(score*10)/10
+        document.getElementById("cursorPrice").innerText = "$ " + cursorPrice
+        document.getElementById("score").innerText = score + " Fish"
+    }
 }
